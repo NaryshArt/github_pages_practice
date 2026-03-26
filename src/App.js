ac47c0bf1edace426cac458d4a2020e9daf093cc
NaryshArt <naryshkin.artyom2005@gmail.com>
2026-03-26 10:38:54 +0400

    Исходники несчастные

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello world!</p>
       </header>
     </div>
+  );
+}
+
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Это страница "Что-то"</p>
+      </header>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <HashRouter>
+      <nav>
+        <Link to="/">Главная</Link>
+        {' | '}
+        <Link to="/about">Что-то</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
